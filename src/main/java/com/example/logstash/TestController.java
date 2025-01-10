36f973a3d88546fb8b3d709c6a1daff88fc65dc1
kmindev <kangkm0324@gmail.com>
2025-01-10 18:58:39 +0900

    spring boot logstash 기능 테스트

---
+++ src/main/java/com/example/logstash/TestController.java
@@ -0,0 +1,17 @@
+package com.example.logstash;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/hello-log")
+    public String helloLog() {
+        log.info("hello log.");
+        return "hello log";
+    }
+
+}
